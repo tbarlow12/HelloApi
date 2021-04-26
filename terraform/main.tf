8aee011eb3093901f6be52987fe66176f1fe9d55
Tess DiStefano <tess.distefano@gmail.com>
2021-04-26 13:30:31 -0700

    Jeff and Tess WIP from other repo

---
+++ terraform/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.52.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "myterraformgroup" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "myterraformnetwork" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+  tags = var.tags
+
+  depends_on = [
+    azurerm_resource_group.myterraformgroup
+  ]
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+
+  depends_on = [
+    azurerm_resource_group.myterraformgroup
+  ]
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.myterraformgroup.name
+  }
+
+  byte_length = 8
+
+  depends_on = [
+    azurerm_resource_group.myterraformgroup
+  ]
+}
+
