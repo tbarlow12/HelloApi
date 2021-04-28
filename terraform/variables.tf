28ccfeb2a0c4c98fcbf5831682adcc36f8de95f6
nemandal <nemandal@microsoft.com>
2021-04-28 11:23:39 -0700

    test run

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,9 +6,6 @@ variable "location" {
 variable "tags" {
   type        = map(string)
   description = "A map of the tags to use for the resources that are deployed"
-  default = {
-    environment = "dev"
-  }
 }
 
 variable "resource_group_name" {
